8b9edb47a3f4e04669f7bb5e5bc19bc105eacb2a
Shaza-Elshimy <shazahamdy011@gmail.com>
2026-05-05 02:10:13 +0300

    feat(register): implement registration form with validation and styling

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  model = {
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    phone: '',
+    address: ''
+  };
+
+  loading = false;
+  error = '';
+
+  register() {
+    this.error = '';
+
+    if (this.model.password !== this.model.confirmPassword) {
+      this.error = "Passwords do not match";
+      return;
+    }
+
+    this.loading = true;
+
+    console.log('Register data:', this.model);
+
+    // بعدين هنربط Django API هنا
+    setTimeout(() => {
+      this.loading = false;
+    }, 1000);
+  }
 }
