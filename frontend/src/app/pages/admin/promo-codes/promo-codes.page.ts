a4450e0099c62aaf4434f95464a7a6afada55cb6
-wessam_khaled5 <wessamkhaled53@gmail.com>
2026-06-06 22:16:37 +0300

    Admin-Front: Generate PromoCodes component for discount management

---
+++ frontend/src/app/pages/admin/promo-codes/promo-codes.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promo-codes',
+  standalone: true,
+  imports: [],
+  templateUrl: './promo-codes.page.html',
+  styleUrl: './promo-codes.page.css'
+})
+export class PromoCodesPage {
+
+}
