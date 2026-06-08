856152afa9d6bef90206bf384e14a32a8085bfb5
nancyjosf <your_email@example.com>
2026-06-08 14:21:30 +0300

    Fix database connection and initialize models for cart, orders, and products

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Categories" 
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products')
+    
+    title = models.CharField(max_length=255) 
+    slug = models.SlugField(max_length=255, unique=True) 
+    description = models.TextField() 
+    
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0) 
+    is_available = models.BooleanField(default=True) 
+    
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
